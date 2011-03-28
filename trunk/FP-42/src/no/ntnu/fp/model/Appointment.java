c0762ef6a398f8e897fab452e265cfd7394ef002
tkjoeglu@gmail.com <tkjoeglu@gmail.com@d192c7cd-6043-23a3-e1a3-ffe06f8194e0>
2011-03-28 14:38:17 +0000

    Laget creator, start, end, description, place og gettere

--- trunk/FP-42/src/no/ntnu/fp/model/Appointment.java
+++ trunk/FP-42/src/no/ntnu/fp/model/Appointment.java
@@ -15,26 +15,71 @@ public class Appointment {
     private int end;
     private String description;
     private String place;
+    private MeetingRoom meetingRoom;
 
-    public String getCreator() {
-        return creator.getName();
+    public Appointment(Person creator, int start, int end, String description, String place) {
+        this.creator = creator;
+        this.start = start;
+        this.end = end;
+        this.description = description;
+        this.place = place;
+    }
+
+    public Appointment(Person creator, int start, int end, MeetingRoom meetingRoom) {
+        this.creator = creator;
+        this.start = start;
+        this.end = end;
+        this.meetingRoom = meetingRoom;
+    }
+
+    
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Person creator) {
+        this.creator = creator;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public int getEnd() {
         return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public MeetingRoom getMeetingRoom() {
+        return meetingRoom;
+    }
+
+    public void setMeetingRoom(MeetingRoom meetingRoom) {
+        this.meetingRoom = meetingRoom;
     }
 
     public String getPlace() {
         return place;
     }
 
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
     public int getStart() {
         return start;
     }
 
-    
+    public void setStart(int start) {
+        this.start = start;
+    }
+
 }
