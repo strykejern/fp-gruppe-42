71d83af7655d6c974c96c4ac91e24b2f1d5bfdd7
omorch@gmail.com <omorch@gmail.com@d192c7cd-6043-23a3-e1a3-ffe06f8194e0>
2011-03-28 12:02:29 +0000

    Laget en spoerring

--- trunk/FP-42/src/Database/DB.java
+++ trunk/FP-42/src/Database/DB.java
@@ -3,6 +3,9 @@
  * and open the template in the editor.
  */
 
+import java.sql.*;
+import java.util.ArrayList;
+
 package Database;
 
 /**
@@ -11,4 +14,46 @@ package Database;
  */
 public class DB {
 
+
+      public static Object getPerson() throws SQLException{
+        final String query = "SELECT * FROM menu ORDER BY dish_id ASC";
+
+        Statement stat = dbConnection.createStatement();
+        stat.executeQuery(query);
+        ResultSet result = stat.getResultSet();
+
+        Menu m = new Menu();
+        while (result.next()){
+            int id          = result.getInt("dish_id");
+            String name     = result.getString("name");
+            int price       = result.getInt("price");
+            String comment  = result.getString("description");
+
+            m.addDish(new Dish(id, name, price, comment));
+        }
+
+        result.close();
+        stat.close();
+
+        return m;
+    }
+
+    public static void addAvtale(Avtale avtaler)
+            throws SQLException {
+
+
+        String query = "INSERT INTO avtale "
+                + "(A_ID, Oppretter, Starttidspunkt, Sluttidspunt, Beskrivelse, Sted, M_ID) VALUES (" +
+                generateA_ID() + ", " +
+                avtaler.moteleder.getName() + ", " +
+                avtaler.start + ", " +
+                avtaler.slutt + ", " +
+                avtaler.beskrivelse + ", " +
+                avtaler.sted + ", " +
+                avtaler.getM_ID() + ",)";
+
+        Statement stat = dbConnection.createStatement();
+
+        stat.executeUpdate(query);
+    }
 }
