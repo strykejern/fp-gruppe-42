aacbb742d2c166baf62dbff942edb24da0ad70c4
tkjoeglu@gmail.com <tkjoeglu@gmail.com@d192c7cd-6043-23a3-e1a3-ffe06f8194e0>
2011-03-28 14:30:24 +0000

    Laget creator, start, end, description, place og gettere

--- trunk/FP-42/src/no/ntnu/fp/model/Appointment.java
+++ trunk/FP-42/src/no/ntnu/fp/model/Appointment.java
@@ -10,5 +10,31 @@ package no.ntnu.fp.model;
  * @author Anders
  */
 public class Appointment {
+    private Person creator;
+    private int start;
+    private int end;
+    private String description;
+    private String place;
 
+    public String getCreator() {
+        return creator.getName();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    
 }
