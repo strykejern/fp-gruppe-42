7f69943c07a41e3602a2b9cd6e13540540192e4f
strykejernene <strykejernene@gmail.com>
2011-03-28 14:08:06 +0000

    added main classes

---
+++ FP-42/src/no/ntnu/fp/model/Meeting.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package no.ntnu.fp.model;
+
+/**
+ *
+ * @author Anders
+ */
+public class Meeting {
+
+}
