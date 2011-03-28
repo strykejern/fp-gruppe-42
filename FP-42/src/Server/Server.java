a0b67e80ea9b92e9856eb64f32487f21ed72efb3
strykejernene <strykejernene@gmail.com>
2011-03-28 12:37:19 +0000

    started implementing Server

--- FP-42/src/Server/Server.java
+++ FP-42/src/Server/Server.java
@@ -5,11 +5,56 @@
 
 package Server;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import no.ntnu.fp.net.MessageListener;
+import no.ntnu.fp.net.ReceiveWorker;
+import no.ntnu.fp.net.co.Connection;
+import no.ntnu.fp.net.co.ConnectionImpl;
+
 /**
  *
  * @author Snorre, Anders og Jan-Tore
  */
-public class Server {
+public class Server implements MessageListener {
+    private ArrayList<ConnectionImpl> listen;
 
+    public Server(){
+        listen = new ArrayList<ConnectionImpl>();
+    }
+
+    public void start(){
+        ConnectionImpl connListener = new ConnectionImpl(200);
+        while(true){
+            try {
+                Connection clientConnection = connListener.accept();
+
+                ReceiveWorker backgroundListener = new ReceiveWorker(clientConnection);
+                backgroundListener.addMessageListener(this); // TODO: lage listener
+                backgroundListener.start();
+
+                listen.add(connListener);
+
+            } catch (IOException e) {
+
+            }
+
+        }
+    }
+
+    public void messageReceived(String message) {
+        for (ConnectionImpl receiver : listen){
+            try {
+                receiver.send(message);
+            } catch (ConnectException ex) {
+                listen.remove(receiver);
+            } catch (IOException ex) {
+            }
+        }
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
