59205ce7a67653099d024caf755c7b97986140ea
tkjoeglu@gmail.com <tkjoeglu@gmail.com@d192c7cd-6043-23a3-e1a3-ffe06f8194e0>
2011-04-04 10:57:02 +0000

    Laget newMeeting() ikke ferdig

--- trunk/FP-42/src/no/ntnu/fp/model/Calendar.java
+++ trunk/FP-42/src/no/ntnu/fp/model/Calendar.java
@@ -34,8 +34,26 @@ public class Calendar {
             Calendar c = new Calendar(bruker);
             return true;
         }
+        return false;
     }
 
-
-
+    public void newMeeting() throws SQLException{
+        int id = meetings.size()+1;
+        Person creator = bruker;
+        Timespan time = new Timespan(/*input fra GUI, dato på avtale/møte*/);
+        String description = /*String fra GUI, beskrivelse av møte/avtale*/"";
+        String place = "";
+        ArrayList meetingRooms = DB.getMeetingRoom(/*input fra GUI, størrelse på ønsket rom*/);
+        MeetingRoom meetingRoom = (MeetingRoom) meetingRooms.get(0);
+        for(int i = 1; i < meetingRooms.size()-1; i++){
+            MeetingRoom nextMeetingRoom = (MeetingRoom) meetingRooms.get(i);
+            if(nextMeetingRoom.getSize()>meetingRoom.getSize()){
+                meetingRoom = nextMeetingRoom;
+            }
+        }
+        if (meetingRoom == null){
+            new Meeting(id, creator, time, description, place);
+        }
+        new Meeting(id, creator, time, description, meetingRoom);
+    }
 }
