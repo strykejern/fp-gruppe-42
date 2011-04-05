b3843c57e85a57a9eea92cb33ee222801f12221f
tkjoeglu@gmail.com <tkjoeglu@gmail.com@d192c7cd-6043-23a3-e1a3-ffe06f8194e0>
2011-04-05 12:51:37 +0000

    Fikset i Person

--- trunk/FP-42/src/no/ntnu/fp/model/Person.java
+++ trunk/FP-42/src/no/ntnu/fp/model/Person.java
@@ -13,81 +13,15 @@ import java.util.Date;
  * @version $Revision: 1.5 $ - $Date: 2005/02/20 14:52:29 $
  */
 public class Person {
-	
-	/**
-	 * This member variable holds the person's name.
-	 */
+
+
 	private String name;
-	
-	/**
-	 * This member variable holds the person's email address.
-	 */
+
 	private String email;
 	
-	/**
-	 * This member variable holds the person's date of birth.
-	 */
-	private Date dateOfBirth;
-	
-	/**
-	 * This member variable holds a unique identifier for this object.
-	 */
-	private long id;
-
         private String username;
 
         private String password;
-
-	
-	/**
-	 * This member variable provides functionality for notifying of changes to
-	 * the <code>Group</code> class.
-	 */
-	private PropertyChangeSupport propChangeSupp;
-	
-	/**
-	 * Constant used when calling 
-	 * {@link java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)}
-	 * on {@linkplain #addPropertyChangeListener(java.beans.PropertyChangeListener) registered
-	 * <code>PropertyChangeListener<code> objecs} when the person's name is changed.
-	 * 
-	 * @see #setName(String) the setName(String) method
-	 */
-	public final static String NAME_PROPERTY_NAME = "name";
-
-	/**
-	 * Constant used when calling 
-	 * {@link java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)}
-	 * on {@linkplain #addPropertyChangeListener(java.beans.PropertyChangeListener) registered
-	 * <code>PropertyChangeListener<code> objecs} when the person's email address is changed.
-	 * 
-	 * @see #setEmail(String) the setEmail(String) method
-	 */
-	public final static String EMAIL_PROPERTY_NAME = "email";
-	
-	/**
-	 * Constant used when calling 
-	 * {@link java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)}
-	 * on {@linkplain #addPropertyChangeListener(java.beans.PropertyChangeListener) registered
-	 * <code>PropertyChangeListener<code> objecs} when the person's date of birth is changed.
-	 * 
-	 * @see #setEmail(String) the setDateOfBirth(java.util.Date) method
-	 */
-	public final static String DATEOFBIRTH_PROPERTY_NAME = "dateOfBirth";
-	
-	/**
-	 * Default constructor. Must be called to initialise the object's member variables.
-	 * The constructor sets the name and email of this person to empty
-	 * {@link java.lang.String}, while the date of birth is given today's date. The 
-	 * {@linkplain #getId() id field} is set to current time when the object is created.
-	 */
-	public Person() {
-		name = "";
-		email = "";
-		dateOfBirth = new Date();
-		id = System.currentTimeMillis();
-		propChangeSupp = new PropertyChangeSupport(this);
-	}
 	
 	/**
 	 * Constructs a new <code>Person</code> object with specified name, email, and date
@@ -97,111 +31,38 @@ public class Person {
 	 * @param email The person's e-mail address
 	 * @param dateOfBirth The person's date of birth.
 	 */
-	public Person(String username, String name, String email) {
-		this();
+	public Person(String username, String name, String email, String password) {
 		this.username = username;
                 this.name = name;
 		this.email = email;
+                this.password = password;
 	}
-	
-	/**
-	 * Assigns a new name to the person.<P>
-	 * 
-	 * Calling this method will invoke the 
-	 * <code>propertyChange(java.beans.PropertyChangeEvent)</code> method on 
-	 * all {@linkplain
-	 * #addPropertyChangeListener(java.beans.PropertyChangeListener) registered
-	 * <code>PropertyChangeListener<code> objecs}.  The {@link java.beans.PropertyChangeEvent}
-	 * passed with the {@link java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)}
-	 * method has the following characteristics:
-	 * 
-	 * <ul>
-	 * <li>the <code>getNewValue()</code> method returns a {@link java.lang.String} 
-	 * with the newly assigned name</li>
-	 * <li>the <code>getOldValue()</code> method returns a {@link java.lang.String} 
-	 * with the person's old name</li>
-	 * <li>the <code>getPropertyName()</code> method returns a {@link java.lang.String} 
-	 * with the value {@link #NAME_PROPERTY_NAME}.</li>
-	 * <li>the <code>getSource()</code> method returns this {@link Person} object
-	 * </ul>
-	 * 
-	 * @param name The person's new name.
-	 *
-	 * @see java.beans.PropertyChangeListener <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/beans/PropertyChangeListener.html">java.beans.PropertyChangeListener</a>
-	 * @see java.beans.PropertyChangeEvent <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/beans/PropertyChangeEvent.html">java.beans.PropertyChangeEvent</a>
-	 */
+
+        /**
+         * Sets the person's name.
+         *
+         * @return void.
+         */
 	public void setName(String name) {
-		String oldName = this.name;
 		this.name = name;
-		PropertyChangeEvent event = new PropertyChangeEvent(this, NAME_PROPERTY_NAME, oldName, name);
-		propChangeSupp.firePropertyChange(event);
 	}
-	
-	/**
-	 * Assigns a new email address to the person.<P>
-	 * 
-	 * Calling this method will invoke the 
-	 * <code>propertyChange(java.beans.PropertyChangeEvent)</code> method on 
-	 * all {@linkplain
-	 * #addPropertyChangeListener(java.beans.PropertyChangeListener) registered
-	 * <code>PropertyChangeListener<code> objecs}.  The {@link java.beans.PropertyChangeEvent}
-	 * passed with the {@link java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)}
-	 * method has the following characteristics:
-	 * 
-	 * <ul>
-	 * <li>the <code>getNewValue()</code> method returns a {@link java.lang.String} 
-	 * with the newly assigned email address</li>
-	 * <li>the <code>getOldValue()</code> method returns a {@link java.lang.String} 
-	 * with the person's old email address</li>
-	 * <li>the <code>getPropertyName()</code> method returns a {@link java.lang.String} 
-	 * with the value {@link #EMAIL_PROPERTY_NAME}.</li>
-	 * <li>the <code>getSource()</code> method returns this {@link Person} object
-	 * </ul>
-	 * 
-	 * @param email The person's new email address.
-	 *
-	 * @see java.beans.PropertyChangeListener <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/beans/PropertyChangeListener.html">java.beans.PropertyChangeListener</a>
-	 * @see java.beans.PropertyChangeEvent <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/beans/PropertyChangeEvent.html">java.beans.PropertyChangeEvent</a>
-	 */
+
+        /**
+         * Sets the person's password.
+         *
+         * @return void.
+         */
+        public void setPassword(String password){
+            this.password = password;
+        }
+
+        /**
+         * Sets the person's email.
+         *
+         * @return void.
+         */
 	public void setEmail(String email) {
-		String oldEmail = this.email;
 		this.email = email;
-		PropertyChangeEvent event = new PropertyChangeEvent(this, EMAIL_PROPERTY_NAME, oldEmail, this.email);
-		propChangeSupp.firePropertyChange(event);
-	}
-	
-	/**
-	 * Assigns a new date of birth to the person.<P>
-	 * 
-	 * Calling this method will invoke the 
-	 * <code>propertyChange(java.beans.PropertyChangeEvent)</code> method on 
-	 * all {@linkplain
-	 * #addPropertyChangeListener(java.beans.PropertyChangeListener) registered
-	 * <code>PropertyChangeListener<code> objecs}.  The {@link java.beans.PropertyChangeEvent}
-	 * passed with the {@link java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)}
-	 * method has the following characteristics:
-	 * 
-	 * <ul>
-	 * <li>the <code>getNewValue()</code> method returns a {@link java.util.Date} 
-	 * with the newly assigned date of birth</li>
-	 * <li>the <code>getOldValue()</code> method returns a {@link java.util.Date} 
-	 * with the person's old date of birth</li>
-	 * <li>the <code>getPropertyName()</code> method returns a {@link java.lang.String} 
-	 * with the value {@link #DATEOFBIRTH_PROPERTY_NAME}.</li>
-	 * <li>the <code>getSource()</code> method returns this {@link Person} object
-	 * </ul>
-	 * 
-	 * @param dateOfBirth The person's new date of birth.
-	 * 
-	 * @see java.beans.PropertyChangeListener <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/util/Date.html">java.util.Date</a>
-	 * @see java.beans.PropertyChangeListener <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/beans/PropertyChangeListener.html">java.beans.PropertyChangeListener</a>
-	 * @see java.beans.PropertyChangeEvent <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/beans/PropertyChangeEvent.html">java.beans.PropertyChangeEvent</a>
-	 */	
-	public void setDateOfBirth(Date dateOfBirth) {
-		Date oldDateOfBirth = this.dateOfBirth;
-		this.dateOfBirth = dateOfBirth;
-		PropertyChangeEvent event = new PropertyChangeEvent(this, DATEOFBIRTH_PROPERTY_NAME, oldDateOfBirth, this.dateOfBirth);
-		propChangeSupp.firePropertyChange(event);
 	}
 	
 	/**
@@ -221,73 +82,24 @@ public class Person {
 	public String getEmail() {
 		return email;
 	}
-	
-	/**
-	 * Returns the person's date of birth.
-	 * 
-	 * @return The person's date of birth.
-	 */
-	public Date getDateOfBirth() {
-		return dateOfBirth;
-	}
-	
-	/**
-	 * Returns this object's unique identification.
-	 * 
-	 * @return The person's unique identification.
-	 */
-	public long getId() {
-		return id;
-	}
 
+        /**
+	 * Returns the person's password.
+	 *
+	 * @return The person's password.
+	 */
         public String getPassword() {
             return password;
         }
 
+        /**
+	 * Returns the person's username.
+	 *
+	 * @return The person's username.
+	 */
         public String getUsername() {
             return username;
         }
-
-
-	/**
-	 * Add a {@link java.beans.PropertyChangeListener} to the listener list.
-	 * 
-	 * @param listener The {@link java.beans.PropertyChangeListener} to be added.
-	 */
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		propChangeSupp.addPropertyChangeListener(listener);
-	}
-	
-	/**
-	 * Remove a {@link java.beans.PropertyChangeListener} from the listener list.
-	 * 
-	 * @param listener The {@link java.beans.PropertyChangeListener} to be removed.
-	 */
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		propChangeSupp.removePropertyChangeListener(listener);
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public boolean equals(Object obj) {
-		if (super.equals(obj))
-			return true;
-		
-		if (obj.getClass() != this.getClass())
-			return false;
-		
-		Person aPerson = (Person)obj;
-		
-		if (aPerson.getName().compareTo(getName()) != 0) 
-			return false;
-		if (aPerson.getEmail().compareTo(getEmail()) != 0)
-			return false;
-		if (aPerson.getDateOfBirth().compareTo(getDateOfBirth()) != 0)
-			return false;
-		
-		return true;
-	}
 	
 	/**
 	 * {@inheritDoc}
@@ -295,7 +107,7 @@ public class Person {
 	public String toString() {
 		String s = "Name: " + getName() + "; ";
 		s += "Email: " + getEmail() + "; ";
-		s += "Date of birth: " + getDateOfBirth().toString();
+		s += "User name: " + getUsername();
 		return s;
 	}
 }
