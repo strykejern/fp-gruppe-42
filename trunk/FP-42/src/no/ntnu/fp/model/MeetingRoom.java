2a1bd67aaa0f4c5ee119d5446071ea78d64e5bab
jan.tore.time@gmail.com <jan.tore.time@gmail.com@d192c7cd-6043-23a3-e1a3-ffe06f8194e0>
2011-04-06 12:22:26 +0000

    Kommentarer

--- trunk/FP-42/src/no/ntnu/fp/model/MeetingRoom.java
+++ trunk/FP-42/src/no/ntnu/fp/model/MeetingRoom.java
@@ -7,7 +7,7 @@ package no.ntnu.fp.model;
 
 /**
  *
- * @author Anders
+ * @author Anders, Jan-Tore, Snorre, Thomas
  */
 public class MeetingRoom {
 
@@ -15,12 +15,29 @@ public class MeetingRoom {
     private String name;
     private int size;
 
+    /*
+     * Konstruktør som tar inn navn og størrelse
+     *
+     * @param name Navnet på møterommet
+     * @param size Størrelsen på møterommet, i.e., hvor mange personer det er plass til
+     *
+     * @return MeetingRoom Møterommet som blir opprettet
+     */
     public MeetingRoom(String name, int size) {
 
         this.name = name;
         this.size = size;
     }
 
+       /*
+     * Konstruktør som tar inn id, navn og størrelse
+     *
+     * @param id Møterommets id
+     * @param name Navnet på møterommet
+     * @param size Størrelsen på møterommet, i.e., hvor mange personer det er plass til
+     *
+     * @return MeetingRoom Møterommet som blir opprettet
+     */
     public MeetingRoom(int id, String name, int size) {
 
         this.id = id;
@@ -29,18 +46,38 @@ public class MeetingRoom {
 
     }
 
+    /*
+     * Metode som returnerer id'en til møterommet
+     *
+     * @return int Id'en til møterommet
+     */
     public int getId() {
         return id;
     }
 
+    /*
+     * Metode som returnerer navnet på møterommet
+     *
+     * @return String Navnet på møterommet
+     */
     public String getName() {
         return name;
     }
 
+    /*
+     * Metode som returnerer størrelsen på møteromnmet
+     *
+     * @param int Størrelsen på møterommet
+     */
     public int getSize() {
         return size;
     }
 
+    /*
+     * Metode som lager en string som beskriver møterommet og returnerer denne
+     *
+     * @return String Stringen som beskriver møtet
+     */
     public String toString() {
         return "ID: " + id + " Navn: " + name + " Size: " + size;
     }
