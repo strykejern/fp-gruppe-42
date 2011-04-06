35af103d16121c2c876e5f82cd79805a20b6a35e
jan.tore.time <jan.tore.time@gmail.com>
2011-04-06 11:54:20 +0000

    Kommentarer

--- FP-42/src/no/ntnu/fp/model/Appointment.java
+++ FP-42/src/no/ntnu/fp/model/Appointment.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 
 /**
  *
- * @author Anders
+ * @author Anders, Jan-Tore, Snorre, Thomas
  */
 public class Appointment {
     private int id;
@@ -21,6 +21,17 @@ public class Appointment {
     private MeetingRoom meetingRoom;
     private int atMeetingRoom;
 
+    /*
+     * Konstruktør som tar inn id, oppretter, tid, beskrivelse og sted
+     *
+     * @param id Avtalens id
+     * @param creator Hvem som oppretter avtalen
+     * @param time Når avtalen
+     * @param description Beskrivelse av avtalen
+     * @param place Hvor avtalen skal være
+     *
+     * @return Appointment Avtalen som ble opprettet
+     */
     public Appointment(int id, Person creator, Timespan time, String description, String place) {
         this.id = id;
         this.creator = creator;
@@ -30,6 +41,17 @@ public class Appointment {
         atMeetingRoom = 0;
     }
 
+    /*
+     * Konstruktør som tar inn id, oppretter, tid, beskrivelse og møterom
+     *
+     * @param id Avtalens id
+     * @param creator Hvem som oppretter avtalen
+     * @param time Når avtalen
+     * @param description Beskrivelse av avtalen
+     * @param møterom Hvor avtalen skal være
+     *
+     * @return Appointment Avtalen som ble opprettet
+     */
     public Appointment(int id, Person creator, Timespan time,String description, MeetingRoom meetingRoom) {
         this.id = id;
         this.creator = creator;
@@ -39,6 +61,16 @@ public class Appointment {
         atMeetingRoom = 1;
     }
 
+    /*
+     * Konstruktør som tar inn oppretter, tid, beskrivelse og møterom
+     *
+     * @param creator Hvem som oppretter avtalen
+     * @param time Når avtalen
+     * @param description Beskrivelse av avtalen
+     * @param meetingRoom Hvor avtalen skal være
+     *
+     * @return Appointment Avtalen som ble opprettet
+     */
     public Appointment(Person creator, Timespan time, String description, MeetingRoom meetingRoom) {
         this.creator = creator;
         this.time = time;
@@ -47,6 +79,16 @@ public class Appointment {
         atMeetingRoom = 1;
     }
 
+    /*
+     * Konstruktør som tar inn oppretter, tid, beskrivelse og sted
+     *
+     * @param creator Hvem som oppretter avtalen
+     * @param time Når avtalen
+     * @param description Beskrivelse av avtalen
+     * @param place Hvor avtalen skal være
+     *
+     * @return Appointment Avtalen som ble opprettet
+     */
     public Appointment(Person creator, Timespan time, String description, String place) {
         this.creator = creator;
         this.time = time;
@@ -55,62 +97,139 @@ public class Appointment {
         atMeetingRoom = 0;
     }
 
-
+    /*
+     * Metode som returnerer id'en til avtalen
+     *
+     * @return int Id'en til avtalen
+     */
     public int getId() {
         return id;
     }
 
+    /*
+     * Metode som setter id'en til avtalen
+     *
+     * @return void
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /*
+     * Metode som returnerer når avtalen skal være
+     *
+     * @return Timespan
+     */
     public Timespan getTime() {
         return time;
     }
 
+    /*
+     * Metode som setter når avtalen skal være
+     *
+     * @return void
+     */
     public void setTime(Timespan time) {
         this.time = time;
     }
 
+    /*
+     * Metode som returnerer personen som har opprettet avtalen
+     *
+     * @return Person Personen som har opprettet avtalen
+     */
     public Person getCreator() {
         return creator;
     }
 
+    /*
+     * Metode som endrer hvem som har opprettet avtalen
+     *
+     * @param creator Personen som har opprettet møtet
+     *
+     * @return void
+     */
     public void setCreator(Person creator) {
         this.creator = creator;
     }
 
+    /*
+     * Metode som returnerer beskrivelsen av avtalen
+     *
+     * @return String Beskrivelsen av møtet
+     */
     public String getDescription() {
         return description;
     }
 
+    /*
+     * Metode som gir avtalen en ny beskrivelse
+     *
+     * @param description Den nye beskrivelsen
+     *
+     * @reutn void
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /*
+     * Metode som returnerer møterommet hvor avtalen skal være
+     *
+     * @return MeetingRoom Møterommet hvor avtalen skal være
+     */
     public MeetingRoom getMeetingRoom() {
         return meetingRoom;
     }
 
+    /*
+     * Metode som endrer på hvor avtalen skal være
+     *
+     * @param meetingRoom
+     *
+     * @return void
+     */
     public void setMeetingRoom(MeetingRoom meetingRoom) {
         this.meetingRoom = meetingRoom;
     }
 
+    /*
+     * Metode som returnerer hvor avtalen skal være
+     *
+     * @return String Hvor avtalen skal være
+     */
     public String getPlace() {
         return place;
     }
 
+    /*
+     * Metode som endrer hvor avtalen skal være
+     *
+     * @param String place
+     *
+     * @return void
+     */
     public void setPlace(String place) {
         this.place = place;
     }
 
+    /*
+     * Metode som returnerer om avtalen er i et møterom
+     *
+     * @return int 1 hvis avtalen er i et møterom, 0 hvis ikke.
+     */
     public int isAtMeetingRoom(){
         return atMeetingRoom;
     }
 
+    /*
+     * Metode som returnerer en string med informasjon om avtalen
+     *
+     * @return String String med informasjon om avtalen
+     */
     public String toString () {
         String room;
-        
+
         if(atMeetingRoom == 1) {
             room = meetingRoom.getName();
         }
