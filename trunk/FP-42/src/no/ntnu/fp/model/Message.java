22619416ccabd39162241b0f47dec3412de40677
omorch@gmail.com <omorch@gmail.com@d192c7cd-6043-23a3-e1a3-ffe06f8194e0>
2011-04-07 13:39:57 +0000

    javadoc

--- trunk/FP-42/src/no/ntnu/fp/model/Message.java
+++ trunk/FP-42/src/no/ntnu/fp/model/Message.java
@@ -11,10 +11,13 @@ package no.ntnu.fp.model;
  */
 public class Message {
 
+
+
     private int id;
     private String subject;
     private String content;
     private String from;
+
 
     public String getFrom() {
         return from;
@@ -24,37 +27,75 @@ public class Message {
         this.from = from;
     }
 
+
+        /*
+     * oppretter en melding
+     * @param subject, meldingsemne
+     * @param content, meldinsgtekst
+     */
     public Message(String subject, String content) {
         this.subject = subject;
         this.content = content;
     }
 
+    /*
+     * oppretter en melding
+     * @param subject, meldingsemne
+     * @param content, meldinsgtekst
+     * @param id, meldingsid
+     */
     public Message(int id, String subject, String content) {
         this.id = id;
         this.subject = subject;
         this.content = content;
     }
 
+    /*
+     * metode som henter meldingsinnhold
+     * @return, meldingsinnhold
+     */
+
     public String getContent() {
         return content;
     }
 
+    /*
+     * metode som endrer meldingsinnhold
+     * @param content, nytt meldingsinnhold
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /*
+     * metode som henter ut meldingsid
+     * @return meldingsid
+     */
     public int getId() {
         return id;
     }
 
+    /*
+     * metode som endrer meldingsid
+     * @param id, ny meldingsid
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /*
+     * metode som henter ut meldingsemne
+     * @return meldingsemne
+     */
     public String getSubject() {
         return subject;
     }
 
+
+    /*
+     * metode som endrer emnet
+     * @param subject, nytt emne
+     */
     public void setSubject(String subject) {
         this.subject = subject;
     }
