3c4fb0f12b339db0f25d6f5ad82efc83d4f4d443
omorch <omorch@gmail.com>
2011-04-07 13:53:46 +0000

    hehe

--- FP-42/src/no/ntnu/fp/model/Appointment.java
+++ FP-42/src/no/ntnu/fp/model/Appointment.java
@@ -102,6 +102,10 @@ public class Appointment {
      *
      * @return int Id'en til avtalen
      */
+    /*
+     * henter ut avtaleid
+     * @return avtaleid
+     */
     public int getId() {
         return id;
     }
